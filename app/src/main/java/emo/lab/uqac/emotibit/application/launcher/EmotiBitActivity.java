fcba86cf76d161f1dde5cff5a6366efaaec9e5cb
SKenshin <35349026+SKenshin@users.noreply.github.com>
2019-01-07 11:00:02 -0500

    page 2

--- app/src/main/java/emo/lab/uqac/emotibit/application/launcher/EmotiBitActivity.java
+++ app/src/main/java/emo/lab/uqac/emotibit/application/launcher/EmotiBitActivity.java
@@ -1,48 +1,52 @@
 package emo.lab.uqac.emotibit.application.launcher;
 
 import android.app.AlertDialog;
+import android.content.BroadcastReceiver;
+import android.content.Context;
 import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.os.BatteryManager;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.view.Window;
 import android.widget.Button;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 
 import emo.lab.uqac.emotibit.R;
 
 public class EmotiBitActivity extends AppCompatActivity {
 
+    private Context mContext;
 
+    private TextView mTextViewPercentage;
+    private ProgressBar mProgressBar;
+    private int mProgressStatus = 0;
 
+    private BroadcastReceiver mBroadcastReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+
+            int scale = intent.getIntExtra(BatteryManager.EXTRA_SCALE,-1);
+
+            int level = intent.getIntExtra(BatteryManager.EXTRA_LEVEL,-1);
+            float percentage = level/ (float) scale;
+            mProgressStatus = (int)((percentage)*100);
+            mTextViewPercentage.setText("" + mProgressStatus + "%");
+            mProgressBar.setProgress(mProgressStatus);
+        }
+    };
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        requestWindowFeature(Window.FEATURE_ACTION_BAR);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(
-                EmotiBitActivity.this);
-
-        // set title
-        alertDialogBuilder.setTitle("Your Title");
-
-        // set dialog message
-        alertDialogBuilder
-                .setMessage("Click yes to exit!")
-                .setCancelable(false)
-                .setPositiveButton("Yes",new DialogInterface.OnClickListener() {
-                    public void onClick(DialogInterface dialog,int id) {
-                        // if this button is clicked, just close
-                        // the dialog box and do nothing
-                        dialog.cancel();
-                        finish();
-                    }
-                });
-
-        // create alert dialog
-        AlertDialog alertDialog = alertDialogBuilder.create();
-
-        // show it
-        alertDialog.show();
-
-
+        mContext = getApplicationContext();
+        IntentFilter iFilter = new IntentFilter(Intent.ACTION_BATTERY_CHANGED);
+        mContext.registerReceiver(mBroadcastReceiver,iFilter);
+        mTextViewPercentage = (TextView) findViewById(R.id.tv_percentage);
+        mProgressBar = (ProgressBar) findViewById(R.id.pb);
     }
-
 }
